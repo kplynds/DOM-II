3482a21d43fbe0e2f44a5042ee5b2d969f347089
Kyle Lynds <kylelynds@Kyles-MBP.lan>
2020-09-29 13:25:36 -0700

    two-tasks-done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
 // Your code goes here
+
+// events to use :
+//   * `mouseover`
+//   * `keydown`
+//   * `wheel`
+//   * `load`
+//   * `focus`
+//   * `resize`
+//   * `scroll`
+//   * `select`
+//   * `dblclick`
+//   * `drag / drop`
+
+// Changes all text color to red
+document.addEventListener("keydown", function (event) {
+    if (event.key === "r") {
+        let links = this.querySelectorAll(".nav-link");
+        document.body.style.color = "red";
+        links.forEach(item => item.style.color = "red");   
+    }
+})
+
+// Changes all text color to blue
+document.addEventListener("keydown", function (event) {
+    if (event.key === "b") {
+        let links = this.querySelectorAll(".nav-link");
+        document.body.style.color = "blue";
+        links.forEach(item => item.style.color = "blue");   
+    }
+})
+
+// Changes home images to a random image on a click
+const images = querySelectorAll(".img-content > img");
+images[0].addEventListener("click", eventListener);
+function randomImage (event) {
+    images[0].src = "https://picsum.photos/200";
+}
+
+images[0].addEventListener("click", randomImage);
